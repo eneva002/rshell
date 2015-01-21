bac2af21d8bd1da2a9f4cbc7d96e20cab6983e3f
Emil Nevarez <eneva002@hammer.cs.ucr.edu>
2015-01-21 00:01:30 -0800

    added code

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+#include <cstdio>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+using namespace std;
 
 int main(int argc, char** argv){
-   std::cout << "hello world!" << std::endl;
+   int pid = fork();
+   if(pid == -1){
+      perror("fork");
+      exit(1);
+   }
+
+   else if(pid == 0){
+       cout << "This is the child process\n ";
+       char temp;
+       cin >> temp;
+       exit(1);
+   }
+   
+   else if(pid > 0){
+      if( -1 == wait(0)) perror("there was an error with wait().");
+   }
+
 }
+
