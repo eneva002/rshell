3fa64312be32a2faa4a9ed84e18684cad80106ad
emil nevarez <eneva002@ucr.edu>
2015-01-23 22:42:48 -0800

    tokenization in progress.

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,29 +1,35 @@
+#include <string>
 #include <iostream>
 #include <unistd.h>
 #include <cstdlib>
 #include <cstdio>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <cstring>
+#include <fstream>
 
 using namespace std;
 
-int main(int argc, char** argv){
-   int pid = fork();
-   if(pid == -1){
-      perror("fork");
-      exit(1);
-   }
+int main(int argc, char* argv[]){
 
-   else if(pid == 0){
-       cout << "This is the child process\n ";
-       char temp;
-       cin >> temp;
-       exit(1);
-   }
-   
-   else if(pid > 0){
-      if( -1 == wait(0)) perror("there was an error with wait().");
-   }
+  ifstream fin("tests.txt");
+  if(!fin.is_open()){
+    cout << "file failed to open" << endl;
+    exit(1);
+  }
 
+  string temp;
+  int i = 0;
+  
+  getline(fin, temp); 
+  //while(getline(fin, temp)){ 
+    char *temp2 = new char [temp.length()+1];
+    std::strcpy(temp2, temp.c_str());
+
+    cout << "========== line " << ++i << " ==========\n";
+    char *token = strtok(temp2, "#"); 
+    cout <<  token << endl;
+    
+    delete temp2;
+  //}
 }
-
