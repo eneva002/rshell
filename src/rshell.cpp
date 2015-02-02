016c70dff36bbb92b74c41ad02f06d42857f6697
emil nevarez <eneva002@ucr.edu>
2015-02-02 14:04:22 -0800

    single line multiple commands mostly working, just have to fix the extra processes spawning

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -9,6 +9,8 @@
 #include <string.h>
 #include <sstream>
 #include <queue>
+#include <sys/types.h>
+#include <signal.h>
 #include <vector>
 
 using namespace std;
@@ -32,18 +34,20 @@ int runcmd(const wordexp_t &result){
   if(pid == 0){
     if(-1 == execvp(result.we_wordv[0], result.we_wordv))
       perror("exec failed");
+      return -1;
   }
   else if(pid < 0){
-    status = -1; 
     perror("fork failed");
+    return -1; 
   }
   else{
     if(waitpid(pid, &status, 0) != pid){
-      status = -1;
       perror("wait failed");
+      kill(pid, SIGKILL);
+      return -1;
     }
   }
-  return status;
+  return 1;
 }
 
 void printprompt(){
@@ -69,14 +73,47 @@ struct Order{
 
 void popq(const string &cmd, queue< pair<string,int> > &cmdq){
   priority_queue<P,vector<P>,Order> list;
+  string piece;
+  P temp;
+  vector<string> look;
+  look.push_back(";");
+  look.push_back("||");
+  look.push_back("&&");
+
+  for(int i = 0; i < 3; ++i){
+    int j = 0;
+    j = cmd.find(look[i], j);
+    while(j != string::npos){ 
+      temp.first = j;
+      switch(i){
+        case 0: 
+          j+=1; break;
+        case 1:
+          j+=2; temp.second = -1; break;
+        case 2:
+          j+=2; temp.second = 1; break;
+      }
+      list.push(temp);
+      j = cmd.find(look[i],j);
+    }
+  }
 
   int i = 0;
-  while(i != string::npos){
-    // find everything!
+  while(!list.empty()){
+    temp = list.top();
+    list.pop();
+    string temp2 = cmd.substr(i, temp.first-i);
+    switch(temp.second){
+      case -1:
+        i = temp.first + 2; break;
+      case 0:
+        i = temp.first + 1; break;
+      case 1:
+        i = temp.first + 2; break;
+    }
+    cmdq.push(make_pair(temp2,temp.second));
   }
-  while(i != string::npos){
-   // i = 
-  }
+  cmdq.push(make_pair(cmd.substr(i, cmd.length()-i),0));
 }
 
 int main(int argc, char* argv[]){
@@ -90,23 +127,34 @@ int main(int argc, char* argv[]){
   while(1){ 
     printprompt(); 
     getline(cin, cmd); 
+    
+    int status = 0;
+    int prevstatus = 0;
+    pair<string, int> o;
+    popq(cmd, cmdq);
+    string temp;
 
-    //get rid of comments
-    int cmnt = cmd.find("#");
-    if(cmnt != string::npos) cmd = cmd.substr(0,cmnt);
+    while(!cmdq.empty()){
+      o = cmdq.front();
+      cmdq.pop();
+      //cout << "\"" << o.first << "\"" << " |:| " << o.second << endl;
+      
+      int cmnt = o.first.find("#");
+      if(cmnt != string::npos) o.first = o.first.substr(0,cmnt);
 
-    //handle exit
-    if(cmd == "exit") exit(0);
+      if(o.first == "exit") exit(0);
 
-    if(-1 == getcmd(cmd, runme)){
-      perror("getcmd failed");
-      exit(1);
+      if(status == prevstatus || prevstatus == 0){
+        if(-1 == getcmd(o.first, runme))
+          perror("getcmd failed");
+        else if(-1 ==(status = runcmd(runme))){
+          perror("runcmd failed");
+        }
+        else wordfree(&runme);
+      }
+      if(o.second) prevstatus = 0;
+      prevstatus = o.second;
     }
-
-    if(-1 == runcmd(runme)){
-      perror("runcmd failed");
-      exit(1);
-    }
-    wordfree(&runme);
   }
- }
+
+}
