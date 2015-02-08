ae4c1e6560f5095f5a7153bda7e7263c103fb487
emil nevarez <eneva002@ucr.edu>
2015-02-08 15:16:43 -0800

    -a flag working

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,14 +1,26 @@
 #include <algorithm>
+#include <ctype.h>
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
 #include <vector>
+#include <list>
 #include <string>
 #include <unistd.h>
 #include <iostream>
 #include <errno.h>
 
 using namespace std;
+
+bool compare_nocase(const string &first, const string &second){
+  unsigned int i = 0;
+  while((i<first.length()) && (i<second.length())){
+    if (tolower(first[i]) < tolower(second[i])) return true;
+    else if (tolower(first[i])>tolower(second[i])) return false;
+    ++i;
+  }
+  return (first.length() < second.length());
+}
 
 int main(int argc, char **argv){
 
@@ -17,7 +29,7 @@ int main(int argc, char **argv){
   if(NULL == getcwd(cur, sz)) perror("getcwd failed");
   cout << "current dir: " << cur << endl;
 
-  vector<string> files;
+  list<string> files;
   char* temp;
 
   DIR *dirp;
@@ -30,11 +42,43 @@ int main(int argc, char **argv){
     files.push_back(string(temp));
     if(errno != 0) { perror("readdir failed"); closedir(dirp); return -1; }
   }
+  files.sort(compare_nocase);
 
-  sort(files.begin(), files.end());
-  for(int i = 0; i < files.size(); ++i){
-    cout << files[i] << endl;
+  int aflag = 0;
+  int lflag = 0;
+  int Rflag = 0;
+  char *xpath = NULL;
+
+  int k;
+  opterr = 0;
+
+  while((k = getopt(argc, argv, "alR")) != -1){
+    switch(k){
+      case 'a':
+        aflag = 1;
+        break;
+      case 'l':
+        lflag = 1;
+        break;
+      case 'R':
+        lflag = 1;
+        break;
+      case '?':
+        perror("getopt failed");
+        return -1;
+      default:
+        abort();
+    }
   }
+  
+  for(list<string>::iterator i = files.begin(); i != files.end(); ++i){
+    if(!aflag){
+      while(*((*i).begin()) == '.') ++i;
+    }
+      
+    cout << *i << "  ";
+  }
+  cout << endl;
 
   closedir(dirp);
 
