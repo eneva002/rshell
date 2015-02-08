5f412dc36a6922c51c343b1e98249ed28b3ac6f8
emil nevarez <eneva002@ucr.edu>
2015-02-08 13:20:31 -0800

    alphebatized

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,18 +1,43 @@
+#include <algorithm>
+#include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <vector>
+#include <string>
 #include <unistd.h>
 #include <iostream>
+#include <errno.h>
 
 using namespace std;
 
 int main(int argc, char **argv){
+
   size_t sz = 256;
   char cur[sz]; 
-  getcwd(cur, sz);
-
+  if(NULL == getcwd(cur, sz)) perror("getcwd failed");
   cout << "current dir: " << cur << endl;
 
-  dirent *CUR;
+  vector<string> files;
+  char* temp;
+
+  DIR *dirp;
+  if((dirp = opendir(cur)) == NULL) perror("opendir failed");
+
+  dirent *direntp;
+  while((direntp = readdir(dirp))){
+    errno = 0;
+    temp = direntp->d_name;
+    files.push_back(string(temp));
+    if(errno != 0) { perror("readdir failed"); closedir(dirp); return -1; }
+  }
+
+  sort(files.begin(), files.end());
+  for(int i = 0; i < files.size(); ++i){
+    cout << files[i] << endl;
+  }
+
+  closedir(dirp);
+
   
   return 0;
 }
